544472e923661e4e89e05a21cc56d2cae2cef6d0
yexingit <72687842+yexin147@users.noreply.github.com>
2022-01-02 22:28:03 +0800

    git提交

--- Basic/src/com/yexin/object/MyObject.java
+++ Basic/src/com/yexin/object/MyObject.java
@@ -2,6 +2,6 @@ package com.yexin.object;
 
 public class MyObject {
     public static void main(String[] args) {
-        System.out.println("面向对象qqq");
+        System.out.println("面向对象qqqgit");
     }
 }
