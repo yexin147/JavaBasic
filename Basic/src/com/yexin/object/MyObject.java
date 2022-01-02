7ae8356553938e2b7c5fa1e216f5c5f73a63df37
叶欣 <72687842+yexin147@users.noreply.github.com>
2022-01-02 22:04:42 +0800

    Update MyObject.java
    3

--- Basic/src/com/yexin/object/MyObject.java
+++ Basic/src/com/yexin/object/MyObject.java
@@ -2,6 +2,6 @@ package com.yexin.object;
 
 public class MyObject {
     public static void main(String[] args) {
-        System.out.println("面向对象qq");
+        System.out.println("面向对象qqq");
     }
 }
