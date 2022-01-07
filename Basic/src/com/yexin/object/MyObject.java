b3f11943f446612110e6dea8ad82f47de3fe4193
yexinjia <lingzwq@163.com>
2022-01-07 23:45:25 +0800

    ctrl+alt+v 接收参数

--- Basic/src/com/yexin/object/MyObject.java
+++ Basic/src/com/yexin/object/MyObject.java
@@ -3,5 +3,8 @@ package com.yexin.object;
 public class MyObject {
     public static void main(String[] args) {
         System.out.println("面向对象qqqgitgit");
+        String a ="aaa";
+        int length = a.length();
+        System.out.println(length);
     }
 }
---
+++ Basic/src/com/yexin/object/NNN.java
@@ -0,0 +1,4 @@
+package com.yexin.object;
+
+public class NNN {
+}
