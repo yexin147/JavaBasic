68b4912182b727d87250844fb7c03c631b6db1bb
yexincom <lingzwq@163.com>
2022-01-17 13:55:42 +0800

    20220117 程序入口类

--- Basic/src/com/yexin/teastumanager/entry/InfoManagerEntry.java
+++ Basic/src/com/yexin/teastumanager/entry/InfoManagerEntry.java
@@ -3,5 +3,6 @@ package com.yexin.teastumanager.entry;
 public class InfoManagerEntry {
     public static void main(String[] args) {
         System.out.println("程序入口类");
+        System.out.println("修改git账号为yexincom");
     }
 }
