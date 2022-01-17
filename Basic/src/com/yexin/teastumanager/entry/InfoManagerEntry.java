37f15e4b84f1c01c8e6f1e8d2482c58e3d271328
yexingit <72687842+yexin147@users.noreply.github.com>
2022-01-17 13:57:57 +0800

    Update InfoManagerEntry.java
    github上提交一下

--- Basic/src/com/yexin/teastumanager/entry/InfoManagerEntry.java
+++ Basic/src/com/yexin/teastumanager/entry/InfoManagerEntry.java
@@ -5,5 +5,6 @@ public class InfoManagerEntry {
         System.out.println("程序入口类");
         System.out.println("修改git账号为yexincom");
         System.out.println("修改git账号为yexincom");
+        System.out.println("github上提交一下");
     }
 }
