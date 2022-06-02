d911ccd67253d89410054b91cc3301013860034a
yexinjia <lingzwq@163.com>
2022-06-02 17:20:57 +0800

    0602 主菜单微调

--- Basic/src/com/yexin/teastumanager/test/StudentManager.java
+++ Basic/src/com/yexin/teastumanager/test/StudentManager.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class StudentManager {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
      lo: while(true){
           System.out.println("---------欢迎来到学生管理系统--------");
           System.out.println("1 添加学生");
@@ -14,26 +14,26 @@ public class StudentManager {
           System.out.println("4 查看学生");
           System.out.println("5 退出");
           System.out.println("请输入您的选择");
-          Scanner sc = new Scanner(System.in);
+
           String a = sc.nextLine();
 //        System.out.println(a);
           switch (a)
           {
               case "1":
-                  System.out.println("1 添加学生");
+                  System.out.println("添加学生");
                   break;
               case "2":
-                  System.out.println("2 删除学生");
+                  System.out.println("删除学生");
                   break;
               case "3":
-                  System.out.println("3 修改学生");
+                  System.out.println("修改学生");
                   break;
               case "4":
-                  System.out.println("4 查看学生");
-                  break;
+                  System.out.println("查看学生");
+                  break;//退出这一层级的循环
               case "5":
-                  System.out.println("5 退出");
-                  break lo;
+                  System.out.println("退出");
+                  break lo;//退出指定的循环
               default:
                   System.out.println("您的输入不正确，请重新输入！");
           }
